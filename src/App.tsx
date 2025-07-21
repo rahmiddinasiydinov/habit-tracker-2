d0bf0ec653424912bc8226fcdc033844a42a48c8
rahmiddin <rahmiddinasiydinov@gmail.com>
2025-07-21 12:08:34 +0500

    feaute: toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Provider } from "react-redux"
 import Rootlayout from "./pages/root-layout"
 import store from "./store"
+import { ThemeProvider } from "./shared/theme/provider"
 
 function App() {
 
   return (
     <>
-    <Provider store={store}>
-      <Rootlayout />
-    </Provider>
+      <Provider store={store}>
+        <ThemeProvider>
+          <Rootlayout />
+        </ThemeProvider>
+      </Provider>
     </>
   )
 }
