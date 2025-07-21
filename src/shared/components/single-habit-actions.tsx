e254485673548e2fbe4a230d17638f8b4d2ad448
rahmiddin <rahmiddinasiydinov@gmail.com>
2025-07-21 11:54:07 +0500

    fix: remove trackings when habit is deleted

--- src/shared/components/single-habit-actions.tsx
+++ src/shared/components/single-habit-actions.tsx
@@ -4,6 +4,7 @@ import { habitActions, type Habit } from '@/store/habit-slice'
 import TrackTodayButton from './track-today-button'
 import EditHabitButton from './edit-habit-button'
 import DeleteHabitButton from './delete-habit-button'
+import { progressActions } from '@/store/progress-slice'
 
 type Props = {
     habit: Habit
@@ -18,6 +19,7 @@ export default function SingleHabitActions({ habit }: Props) {
 
     const handleDelete = () => {
         dispatch(habitActions.deleteHabit(habit.id));
+        dispatch(progressActions.removeHabitTrackings(habit.id));
     }
 
     return (
--- src/store/progress-slice.ts
+++ src/store/progress-slice.ts
@@ -36,11 +36,13 @@ const progressSlice = createSlice({
         addTracking(state: ProgressSliceValue, action: AddActionType) {
             state.progress.push(action.payload);
         },
-        
         removeTracking(state: ProgressSliceValue, action: RemoveActionType) {
             const id = action.payload;
             const currentTrackIndex = state.progress.findIndex((progress: SingleProgressValue) => progress.id === id);
             state.progress.splice(currentTrackIndex, 1);
+        },
+        removeHabitTrackings(state, action: { type: string, payload: string }) {
+            state.progress = state.progress.filter((progress) => progress.habitId !== action.payload);
         }
     }
 })
