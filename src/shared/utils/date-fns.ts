0b081fc92d646414cdebabdb288c1144216de9a0
rahmiddin <rahmiddinasiydinov@gmail.com>
2025-07-22 12:50:13 +0500

    finish project

--- src/shared/utils/date-fns.ts
+++ src/shared/utils/date-fns.ts
@@ -13,9 +13,3 @@ export const getAreDatesEquel = (date1: string, date2: string) => {
 
     return areDatesEquel;
 }
-
-// const result = [
-//   new Date(1995, 6, 2),
-//   new Date(1987, 1, 11),
-//   new Date(1989, 6, 10)
-// ].sort(compareDesc)
