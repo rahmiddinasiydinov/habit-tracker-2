aca2f7279e212480849b097a2d6872bfa79933ba
rahmiddin <rahmiddinasiydinov@gmail.com>
2025-07-29 15:11:49 +0500

    refactor: remove lazyloading for home, app, error pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,19 @@
+import App from '@/App';
+import ErrorPage from '@/pages/error';
+import Home from '@/pages/home';
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-const App = lazy(() => import('@/App'));
-const Home = lazy(() => import('@/pages/home'));
 const StatsPage = lazy(() => import('@/pages/stats'));
 const HabitDetailPage = lazy(() => import('@/pages/habit-detail'));
-const ErrorPage = lazy(() => import('@/pages/error'));
 const SettingsPage = lazy(() => import('@/pages/settings'));
 
 export const router = createBrowserRouter([
     {
-        path: '/', element: <App/>,
+        path: '/', element: <App />,
         errorElement: <ErrorPage />,
         children: [
-            { index: true, element: <Home/> },
+            { index: true, element: <Home /> },
             { path: 'stats', element: <StatsPage /> },
             { path: 'habit/:id', element: <HabitDetailPage /> },
             { path: 'settings', element: <SettingsPage /> }
